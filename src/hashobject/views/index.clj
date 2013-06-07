6fcda16f7af2ad5003291c5654682c29721ba180
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-06 22:13:18 -0600

    add google analytics

--- src/hashobject/views/index.clj
+++ src/hashobject/views/index.clj
@@ -3,6 +3,15 @@
         [hiccup.page :only (html5 include-css include-js)]))
 
 
+(defn ga[]
+  [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41533076-1', 'hashobject.com');
+  ga('send', 'pageview');"])
+
 
 (defn index []
   (html5 {:lang "en"}
@@ -14,5 +23,6 @@
       [:meta {:name "keywords" :itemprop "keywords" :content "HashObject, hashobject, #{hash:object}, HashObject Team, HashObject Ltd"}]
       [:meta {:name "description" :itemprop "description" :content "HashObject - software engineering, design and application development"}]
       [:title "HashObject: we love development"]
-      (include-css "/css/app.css")]
+      (include-css "/css/app.css")
+      (ga)]
     [:body "Our site will be here soon..."]     ))
