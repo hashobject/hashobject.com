b8f2d7a39359553bfd7c5d1db2c453baf225a63b
Anton Podviaznikov <podviaznikov@gmail.com>
2014-12-22 23:14:55 -0800

    update deps

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "http://hashobject.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [hiccup "1.0.3"]]
-  :plugins [[lein-shell "0.1.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-shell "0.4.0"]]
   :aliases {"html" ["trampoline" "run" "-m" "hashobject.generator/generate"]
-            "site-build" ["shell" "./frontend/node_modules/grunt-cli/bin/grunt" "--gruntfile" "frontend/Gruntfile.js" "build"]
-            "site-deploy" ["shell" "./frontend/node_modules/grunt-cli/bin/grunt" "--gruntfile" "frontend/Gruntfile.js" "deploy"]})
+            "site-deploy" ["shell" "gulp" "deploy"]})
